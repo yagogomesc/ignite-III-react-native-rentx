cac1708d2739013ab6f4f074ad27af4444de49aa
Yago Gomes <yagogomesc@gmail.com>
2022-06-30 21:06:09 -0300

    Cria home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,48 @@
 import React from "react";
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
 
-import { Container, Title } from "./styles";
+import Logo from "../../assets/logo.svg";
+import { Car } from "../../components/Car";
+
+import { Container, Header, TotalCars, HeaderContent } from "./styles";
 
 export function Home() {
+  const carDataOne = {
+    brand: "Audi",
+    name: "RS 5 Coupé",
+    rent: {
+      period: "Ao dia",
+      price: 120,
+    },
+    thumbnail: "https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png",
+  };
+  const carDataTwo = {
+    brand: "Porsche",
+    name: "Panamera",
+    rent: {
+      period: "Ao dia",
+      price: 340,
+    },
+    thumbnail:
+      "https://www.pngkit.com/png/full/237-2375888_porsche-panamera-s.png",
+  };
+
   return (
     <Container>
-      <Title>Home</Title>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+      <Header>
+        <HeaderContent>
+          <Logo height={RFValue(12)} width={RFValue(108)} />
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
+      <Car data={carDataOne} />
+      <Car data={carDataTwo} />
     </Container>
   );
 }
