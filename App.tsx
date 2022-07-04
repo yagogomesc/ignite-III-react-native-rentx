fbbce6e6ff015d4973f1ce2f177687683ec5ece1
Yago Gomes <yagogomesc@gmail.com>
2022-07-04 18:56:33 -0300

    Ajuste para visualizar tela de detalhes

--- App.tsx
+++ App.tsx
@@ -14,6 +14,7 @@ import { Home } from "./src/screens/Home";
 import AppLoading from "expo-app-loading";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/styles/theme";
+import { CarDetails } from "./src/screens/CarDetails";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -30,7 +31,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <CarDetails />
     </ThemeProvider>
   );
 }
